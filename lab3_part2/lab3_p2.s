a8e40b4deb58beb8af34447f49b25e430f7b0266
Andthehand <sixdefords50@gmail.com>
2024-09-10 00:31:05 -0400

    Added assembly function comments

--- lab3_part2/lab3_p2.s
+++ lab3_part2/lab3_p2.s
@@ -23,16 +23,63 @@
 .global my_asm_16bitclr
 .global my_asm_16bitcheck
 
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function sets the specified bit(s) in a 16-bit register value using
+//  the provided bit mask. It performs a bitwise OR operation to set the bit(s).
+//
+// INPUT PARAMETERS:
+//  reg_value - The original 16-bit register value.
+//  bit_mask  - The bit mask indicating which bit(s) to clear.
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+//  uint16_t - The modified register value with the specified bit(s) set.
+// -----------------------------------------------------------------------------
 my_asm_16bitset:
   ORRS R0, R1
   BX LR
 
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function clears the specified bit(s) in a 16-bit register value using
+//  the provided bit mask. It performs a bitwise AND operation with the
+//  complement of the bit mask to clear the bit(s).
+//
+// INPUT PARAMETERS:
+//  reg_value - The original 16-bit register value.
+//  bit_mask  - The bit mask indicating which bit(s) to clear.
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+//  uint16_t - The modified register value with the specified bit(s) cleared.
+// -----------------------------------------------------------------------------
 my_asm_16bitclr:
   //~bitmask
   MVNS R0, R0
   ANDS R0, R1
   BX LR
 
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function checks if the specified bit(s) in a 16-bit register value are
+//  set using the provided bit mask. It performs a bitwise AND operation to 
+//  verify if the bit(s) are set.
+//
+// INPUT PARAMETERS:
+//  reg_value - a 16-bit register value to check.
+//  bit_mask  - a 16-bit mask indicating which bit(s) to check.
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+//  bool - true if the specified bit(s) are set, false otherwise.
+// -----------------------------------------------------------------------------
 my_asm_16bitcheck:
   ANDS R0, R1
   BX LR
