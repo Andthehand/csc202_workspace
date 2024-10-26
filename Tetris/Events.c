46f1485e066b2d25b7ffc03da24d7058339eaef3
Andthehand <sixdefords50@gmail.com>
2024-10-25 22:59:37 -0400

    Added Events

---
+++ Tetris/Events.c
@@ -0,0 +1,138 @@
+// *****************************************************************************
+// ***************************    C Source Code     ****************************
+// *****************************************************************************
+//   DESIGNER NAME:  Andrew DeFord
+//
+//         VERSION:  0.1
+//
+//       FILE NAME:  Event.c
+//
+//-----------------------------------------------------------------------------
+// DESCRIPTION
+//    
+//
+// NOTES:
+//    
+//
+// *****************************************************************************
+//******************************************************************************
+
+
+//-----------------------------------------------------------------------------
+// Load standard C include files
+//-----------------------------------------------------------------------------
+#include <math.h>
+#include <stdint.h>
+
+//-----------------------------------------------------------------------------
+// Loads MSP launchpad board support macros and definitions
+//-----------------------------------------------------------------------------
+#include <ti/devices/msp/msp.h>
+#include "Events.h"
+
+//-----------------------------------------------------------------------------
+// Define global variables and structures here.
+// NOTE: when possible avoid using global variables
+//-----------------------------------------------------------------------------
+bool g_PB1_Pressed = false;
+bool g_PB2_Pressed = false;
+
+void init_PB1_irq()
+{
+  uint32_t gpio_pincm = IOMUX_PINCM_INENA_ENABLE | IOMUX_PINCM_PC_CONNECTED |
+                    PINCM_GPIO_PIN_FUNC | IOMUX_PINCM_INV_ENABLE;
+  IOMUX->SECCFG.PINCM[PB1_IOMUX] = gpio_pincm;
+
+  GPIOA->POLARITY15_0 |= GPIO_POLARITY15_0_DIO11_RISE;
+  GPIOA->CPU_INT.ICLR &= ~GPIO_CPU_INT_ICLR_DIO11_CLR;
+  GPIOA->CPU_INT.IMASK |= GPIO_CPU_INT_IMASK_DIO11_SET;
+}
+
+void init_PB2_irq()
+{
+  uint32_t gpio_pincm = IOMUX_PINCM_INENA_ENABLE | IOMUX_PINCM_PC_CONNECTED |
+                    PINCM_GPIO_PIN_FUNC | IOMUX_PINCM_INV_ENABLE;
+  IOMUX->SECCFG.PINCM[PB2_IOMUX] = gpio_pincm;
+
+  GPIOA->POLARITY15_0 |= GPIO_POLARITY15_0_DIO12_RISE;
+  GPIOA->CPU_INT.ICLR &= ~GPIO_CPU_INT_ICLR_DIO12_CLR;
+  GPIOA->CPU_INT.IMASK |= GPIO_CPU_INT_IMASK_DIO12_SET;
+}
+
+void init_events(void)
+{
+  init_PB1_irq();
+  init_PB2_irq();
+
+  NVIC_SetPriority(GPIOA_INT_IRQn, 2);
+  NVIC_EnableIRQ(GPIOA_INT_IRQn);
+}
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This code snippet can handle interrupts from two GPIO groups (GPIOA and GPIOB) 
+//  in a loop, checking for specific interrupt statuses.
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void GROUP1_IRQHandler(void)
+{
+  uint32_t group_iidx_status;
+  uint32_t gpio_mis;
+
+  do
+  {
+    group_iidx_status = CPUSS->INT_GROUP[1].IIDX;
+
+    switch (group_iidx_status) 
+    {
+      case (CPUSS_INT_GROUP_IIDX_STAT_INT0): //GPIOA
+        gpio_mis = GPIOA->CPU_INT.MIS;
+        
+        if((gpio_mis & GPIO_CPU_INT_MIS_DIO11_MASK) == GPIO_CPU_INT_MIS_DIO11_SET)
+        {
+          g_PB1_Pressed = true;
+          GPIOA->CPU_INT.ICLR = GPIO_CPU_INT_ICLR_DIO11_CLR;
+        } /* if */
+        else if ((gpio_mis & GPIO_CPU_INT_MIS_DIO12_MASK) == GPIO_CPU_INT_MIS_DIO12_SET) 
+        {
+          g_PB2_Pressed = true;
+          GPIOA->CPU_INT.ICLR = GPIO_CPU_INT_ICLR_DIO12_CLR;
+        }
+        break;
+    } /* switch */
+  } while(group_iidx_status != 0);
+} /* GROUP1_IRQHandler */
+
+bool is_pb1_down(bool handled) 
+{
+  bool temp = g_PB1_Pressed;
+
+  if(handled && g_PB1_Pressed) 
+  {
+    g_PB1_Pressed = false;
+    msec_delay(DEBOUNCE_DELAY);
+  }
+
+  return temp;
+}
+
+bool is_pb2_down(bool handled) 
+{
+  bool temp = g_PB2_Pressed;
+
+  if(handled && g_PB2_Pressed) 
+  {
+    g_PB2_Pressed = false;
+    msec_delay(DEBOUNCE_DELAY);
+  }
+
+  return temp;
+}
--- Tetris/main.c
+++ Tetris/main.c
@@ -24,17 +24,17 @@
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
-#include "clock.h"
-#include "ST7735S.h"
-#include "LaunchPad.h"
-#include "Joystick.h"
-#include "uart.h"
 
+#include "ST7735S.h"
+#include "Joystick.h"
+#include "Events.h"
+
+#include "LaunchPad.h"
+#include "clock.h"
 
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
-void msp_printf(char* buffer, unsigned int value);
 
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
@@ -53,23 +53,23 @@ int main(void)
 {
   clock_init_40mhz();
   launchpad_gpio_init();
-  UART_init(115200);
 
   ST7735S_init();
-
   joystick_init();
 
+  init_events();
+
+  bool pressed = false;
   //Game loop
   while (true) {
     ST7735S_set_addr(0, 0);
     ST7735S_write_command(LCD_RAMWR_CMD);
+    if (is_pb1_down(true)) {
+      pressed = !pressed;
+    }
     for (uint16_t i = 0; i < (128 * 128); i++)
     {
-      ST7735S_write_color(RGB_to_color(0xFF, 0x00, 0xFF));
-      joystick_pos pos = read_joystick();
-      msp_printf("Joystick X pos: %u\r\n", pos.x_pos);
-      msp_printf("Joystick Y pos: %u\r\n", pos.y_pos);
-      msec_delay(100);
+      ST7735S_write_color(RGB_to_color(pressed ? 0x00 : 0xFF, 0x00, 0xFF));
     }
   }
 
@@ -77,34 +77,3 @@ int main(void)
   while (1);
 
 } /* main */
-
-
-//-----------------------------------------------------------------------------
-// DESCRIPTION:
-//  A custom implementation of printing using the MSPM0 with a printf format
-//  
-// INPUT PARAMETERS: 
-//  buffer: A format string using the same format as sprintf.
-//  value: The integer value to be formatted into the output string.
-//  
-// OUTPUT PARAMETERS:
-//  none 
-//
-// RETURN:
-//  none
-// 
-//-----------------------------------------------------------------------------
-void msp_printf(char* buffer, unsigned int value) {
-  unsigned int i = 0;
-  unsigned int len = 0;
-  char string[80];
-
-  len = sprintf(string, buffer, value);
-
-  // Sends the string one bit at a time through UART
-  for(i = 0; i < len; i++)
-  {
-    UART_out_char(string[i]);
-  } /* for */
-
-} /* msp_printf */
