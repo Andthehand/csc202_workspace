2ca02bbad9fb6c2e0f71700febe13d0aa72f79c8
Andthehand <sixdefords50@gmail.com>
2024-11-19 11:11:51 -0500

    Lab 11 working

--- lab11_p1/lab11p1_main.c
+++ lab11_p1/lab11p1_main.c
@@ -3,9 +3,9 @@
 //*****************************************************************************
 //  DESIGNER NAME:  Andrew DeFord
 //
-//       LAB NAME:  Lab 10
+//       LAB NAME:  Lab 11
 //
-//      FILE NAME:  main_part1.c
+//      FILE NAME:  lab11p1_main.c
 //
 //-----------------------------------------------------------------------------
 //
@@ -28,7 +28,6 @@
 #include "lcd1602.h"
 #include "clock.h"
 #include "uart.h"
-#include "adc.h"
 #include "spi.h"
 
 
@@ -39,7 +38,10 @@ void msp_printf(char* string);
 
 void run_lab11_part1();
 
-void get_spi_data();
+uint8_t get_spi_data();
+
+uint16_t string_to_uint16(char* string);
+void uint16_to_string(uint16_t value, char* string);
 
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
@@ -54,23 +56,25 @@ void get_spi_data();
 #define PART1_STRING_THANKS               "Thank you for using the program\r\n"
 #define PART1_STRING_NOT_VALID             "ERROR: Your input is not valid\r\n"
 #define PART1_STRING_NEW_LINE                                            "\r\n"
+#define PART1_STRING_SEE_SERIAL                               "See serial port"
+#define PART1_STRING_START                                    "Program running"
+#define PART1_STRING_END                                      "Program Stopped"
 
 #define PART1_STRING_XMIT                 "Set SPI xmit data menu selected\r\n"
 #define PART1_STRING_ENTER_VAL      "Enter a valid decimal number (0 to 255): "
 #define PART1_STRING_SPI_SET                                "SPI Data set to: "
-#define PART2_STRING_END                                      "Program Stopped"
+#define PART1_STRING_SEND                                              "SEND: "
+#define PART1_STRING_RECV                                              "RECV: "
 
 #define PART1_CHAR_DEGREE                                                  0xDF
 #define PART1_CHAR_FAHRENHEIT                                               'F'
 #define PART1_CHAR_END_STRING                                              '\0'
-#define PART1_CHAR_END_LINE                                                '\n'
+#define PART1_CHAR_RETURN                                                  '\r'
+#define PART1_CHAR_BACKSPACE                                               '\b'
 #define PART1_CHAR_ZERO                                                     '0'
 
-#define PART1_FAHRENHEIT_CONVERSTION                             9.0 / 5.0 + 32
-#define PART1_CHANNEL_TEMP                                                    5
-#define PART1_LCD_MAX                                                         9
-#define PART1_BLINK_COUNT                                                     3
 #define PART1_BLINK_DELAY                                                   250
+#define BUFFER_SIZE                                                           6
 
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
@@ -86,14 +90,15 @@ int main(void)
   launchpad_gpio_init();
   UART_init(115200);
 
-  led_init();
-  seg7_init();
   spi1_init();
-
-  ADC0_init(ADC12_MEMCTL_VRSEL_VDDA_VSSA);
 
   I2C_init();
   lcd1602_init();
+
+  IOMUX->SECCFG.PINCM[LP_SPI_CS0_IOMUX] = (IOMUX_PINCM_PC_CONNECTED |
+                    PINCM_GPIO_PIN_FUNC);
+  GPIOB->DOE31_0 |= LP_SPI_CS0_MASK;
+  GPIOB->DOUT31_0 &= ~LP_SPI_CS0_MASK;
 
   run_lab11_part1();
   
@@ -139,8 +144,14 @@ void msp_printf(char* string) {
 // -----------------------------------------------------------------------------
 void run_lab11_part1()
 {
-  leds_on(0xFF);
-  seg7_hex(0, SEG7_DIG0_ENABLE_IDX);
+  lcd_set_ddram_addr(LCD_LINE1_ADDR);
+  lcd_write_string(PART1_STRING_START);
+
+  lcd_set_ddram_addr(LCD_LINE2_ADDR);
+  lcd_write_string(PART1_STRING_SEE_SERIAL);
+
+  uint8_t spi_data = 0;
+  char str[BUFFER_SIZE];
 
   bool finished = false;
   while (!finished) {
@@ -157,11 +168,26 @@ void run_lab11_part1()
     msp_printf(PART1_STRING_NEW_LINE);
     switch (input) {
       case '1':
-        get_spi_data();
+        spi_data = get_spi_data();
         break;
       case '2':
+        lcd_clear();
+
+        lcd_set_ddram_addr(LCD_LINE1_ADDR);
+        lcd_write_string(PART1_STRING_SEND);
+        uint16_to_string(spi_data, str);
+        lcd_write_string(str);
+        spi1_write_data(spi_data);
+
+        lcd_set_ddram_addr(LCD_LINE2_ADDR);
+        lcd_write_string(PART1_STRING_RECV);
+        uint16_to_string(spi1_read_data(), str);
+        lcd_write_string(str);
         break;
       case '3':
+        GPIOB->DOUT31_0 |= LP_SPI_CS0_MASK;
+        msec_delay(PART1_BLINK_DELAY);
+        GPIOB->DOUT31_0 &= ~LP_SPI_CS0_MASK;
         break;
       case '4':
         msp_printf(PART1_STRING_THANKS);
@@ -173,16 +199,15 @@ void run_lab11_part1()
     } 
   }
 
-  seg7_off();
-  leds_off();
-
+  lcd_clear();
   lcd_set_ddram_addr(LCD_LINE1_ADDR);
   lcd_write_string(PART1_STRING_END);
 }
 
 //-----------------------------------------------------------------------------
 // DESCRIPTION:
-//  This function blinks all LEDs a specified number of times.
+//  This function stores the data that we want to send through spi from the UART
+//  in functions.
 //
 // INPUT PARAMETERS:
 //    none
@@ -193,37 +218,123 @@ void run_lab11_part1()
 // RETURN:
 //    none
 // -----------------------------------------------------------------------------
-void get_spi_data()
+uint8_t get_spi_data()
 {
   msp_printf(PART1_STRING_XMIT);
   msp_printf(PART1_STRING_ENTER_VAL);
 
-
-  uint32_t number = 0;
+  char full_num[BUFFER_SIZE];
+  uint8_t index = 0;
   do {
     char input = UART_in_char();
-    number = input - PART1_CHAR_ZERO
-  }while (input != PART1_CHAR_END_LINE);
+    UART_out_char(input);
+
+    if(input == PART1_CHAR_RETURN)
+    {
+      full_num[index] = PART1_CHAR_END_STRING;
+      index++;
+    }
+    else if (input == PART1_CHAR_BACKSPACE) 
+    {
+      index--;
+    }
+    else if (index < (sizeof(full_num) / sizeof(full_num[0])) - 1) {
+      full_num[index] = input;
+      index++;
+    }
+  }while (full_num[index - 1] != PART1_CHAR_END_STRING);
 
   msp_printf(PART1_STRING_NEW_LINE);
-  msp_printf(PART1_STRING_NEW_LINE);
 
-  if(number <= 255)
+  uint16_t number = string_to_uint16(full_num);
+  if(number <= UINT8_MAX)
   {
-    spi1_write_data(number);
-
-    msp_printf(PART1_STRING_SPI_SET + number);
-    msp_printf(PART1_STRING_NOT_VALID);
+    msp_printf(PART1_STRING_SPI_SET);
+    msp_printf(full_num);
     msp_printf(PART1_STRING_NEW_LINE);
     msp_printf(PART1_STRING_NEW_LINE);
   }
   else 
   {
-    spi1_write_data(0);
+    number = 0;
 
-    msp_printf(PART1_STRING_NOT_VALID)
-    msp_printf(PART1_STRING_SPI_SET + '0');
+    msp_printf(PART1_STRING_NOT_VALID);
+    msp_printf(PART1_STRING_SPI_SET);
+    UART_out_char(PART1_CHAR_ZERO);
     msp_printf(PART1_STRING_NEW_LINE);
     msp_printf(PART1_STRING_NEW_LINE);
   }
+
+  return (uint8_t)number;
 }
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function converts a uint16_t to a string
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+uint16_t string_to_uint16(char* string)
+{
+  uint16_t number = 0;
+  uint16_t index = 0;
+  while (string[index] != PART1_CHAR_END_STRING)
+  {
+    number = number * 10 + (string[index] - PART1_CHAR_ZERO);
+    index++;
+  } /* while */
+
+  return number;
+}
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function converts a string to a uint16_t
+//
+// INPUT PARAMETERS:
+//    none
+//
+// OUTPUT PARAMETERS:
+//    none
+//
+// RETURN:
+//    none
+// -----------------------------------------------------------------------------
+void uint16_to_string(uint16_t number, char *buffer)
+{
+  // Handle the special case of 0
+  if (number == 0)
+  {
+    buffer[0] = PART1_CHAR_ZERO;
+    buffer[1] = PART1_CHAR_END_STRING;
+  }
+  else
+  {
+    // Start from the end of the buffer
+    uint8_t index = BUFFER_SIZE - 1;
+    buffer[index] = PART1_CHAR_END_STRING; // Null-terminate the string
+    // Convert the number to string in reverse order 
+    while (number > 0)
+    {
+      index--;
+      // Convert last digit to character and stuff into buffer 
+      buffer[index] = (number % 10) + PART1_CHAR_ZERO;
+      // Remove the last digit and update number
+      number /= 10;
+    } /* while */
+    
+    // Loop above ended at index, so first character is at buffer[index]
+    // so slide string to left in buffer as needed
+    for (uint8_t i = index; i < BUFFER_SIZE; i++)
+    {
+      buffer[i - index] = buffer[i];
+    } /* for */
+  } /* else */
+} /* uint16_to_string */
